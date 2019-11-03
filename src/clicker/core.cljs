0fc4fe0c805912335cbbd800095a0f36217e748b
Stephen Cagle <stephen@read-line.com>
2019-11-03 15:05:00 -0800

    Show one row per participant in our game

--- src/clicker/core.cljs
+++ src/clicker/core.cljs
@@ -10,12 +10,36 @@
 
 (defonce app-state (reagent/atom {:text "Hello world!"}))
 
+(defn n-from-hash-of [o]
+  (let [n (* 10 1000)]
+    (-> o hash (mod n) (/ n))))
+
+;; https://stackoverflow.com/questions/43193341/how-to-generate-random-pastel-or-brighter-color-in-javascript
+(defn pastel [o]
+  (let [n (n-from-hash-of o)]
+    (str "hsl(" (* 360 n) ",70%,85%)")))
 
 (defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]
-   [mui/button "My first button"]])
+  [:<>
+   [mui/app-bar {:position :fixed :style {:flex-grow 1}}
+    [mui/toolbar
+     [mui/icon-button {:edge :start :color :inherit}
+      [mui/icon "menu"]]
+     [mui/typography {:variant :h6 :style {:flex-grow 1}}
+      "Clicker"]
+     [mui/button {:color :inherit} "Login"]]]
+
+   [mui/container {:style {:margin-top "5em"}}
+    [mui/grid-list {:cell-height 120  :cols 1}
+     (for [sym '(I am the walrus Goo goo g'joob)
+           :let [s (str sym)]]
+       [mui/grid-list-tile {:key s
+                            :style {:background-color (pastel s)}}
+        [mui/grid-list-tile-bar
+         {:title (str "A study in '" s "'")
+          :subtitle (str "by: " s "-kun")}]
+        [mui/typography s]])]]])
+
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
